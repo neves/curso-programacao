a4a79664abcc0e14c7aeb499e5a52f9d868957a5
Marcos Neves <marcos.neves@gmail.com>
2010-02-13 05:34:30 -0200

    imposto.rb

---
+++ algoritmos/ruby/condicional/if/imposto.rb
@@ -0,0 +1,16 @@
+#!/bin/env ruby
+
+=begin
+titulo: Cálculo de Imposto
+enunciado: Criar um programa que receba o seu salário e valor total de bens adiquiridos,
+	e diga se a pessoa terá que pagar imposto ou será isento. Se tiver que pagar, exibir o valor do imposto,
+	como sendo 0.99% sobre o lucro total no ano e 15.764,28 o valor máximo para ser considerado isento.
+exemplos: 
+  1: Area do quadrado de lado 1 = 1
+  2: Area do quadrado de lado 2 = 2
+	9: Area do quadrado de lado 9 = 81
+dificuldade: 1
+linguagem: ruby
+solucao: Utilizando o operador de potência **
+categorias: [matematica]
+=end
